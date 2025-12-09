033622d86a7dedfbf1366d39bda0f7262c7154a5
Ashish2033 <Ashishsalian24@gmail.com>
2025-12-09 16:18:31 +0530

    initial commit

---
+++ models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^\S+@\S+\.\S+$/, "Please use a valid email address"],
+    },
+
+
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true, // Adds createdAt and updatedAt fields
+  }
+);
+
+
+const User = mongoose.model("User", userSchema);
+
+
+module.exports = User;
